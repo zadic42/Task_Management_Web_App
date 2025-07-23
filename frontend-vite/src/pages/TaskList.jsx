98aa5582547e74a5216c959ae05fca6bfec98ec0
Mohammed sadik <130451993+zadic42@users.noreply.github.com>
2025-07-23 10:55:20 +0530

    Update TaskList.jsx

--- frontend-vite/src/pages/TaskList.jsx
+++ frontend-vite/src/pages/TaskList.jsx
@@ -18,7 +18,7 @@ function TaskList() {
 
   const fetchTasks = async () => {
     try {
-      const res = await axios.get('http://localhost:5000/api/tasks', {
+      const res = await axios.get('https://task-management-web-app-backend-1ddz.onrender.com/api/tasks', {
         headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
       });
       setTasks(res.data);
